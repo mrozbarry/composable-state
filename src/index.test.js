3e97ae9c12a0322364e327ada675d81a601fc93e
Alex Barry <alex@Alexs-MacBook-Pro.local>
2021-04-04 08:36:34 -0400

    Add range action

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
-import deepImmutable, {
+import {
+  compose,
   merge,
   concat,
   replace,
@@ -8,64 +9,66 @@ import deepImmutable, {
   setIn,
   collect,
   map,
+  splice,
+  range,
 } from './index.js';
 
 test('can update a top level variable', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     { text: 'hello' },
     merge({ text: 'goodbye' }),
   );
 
-  t.deepEqual(updated, { text: 'goodbye' });
+  t.deepEqual(state, { text: 'goodbye' });
 });
 
 test('merge preserves items', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     { foo: [], text: 'hello' },
     merge({ text: 'goodbye' }),
   );
 
-  t.deepEqual(updated, { foo: [], text: 'goodbye' });
+  t.deepEqual(state, { foo: [], text: 'goodbye' });
 });
 
 test('replace completely changes the object', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     { foo: [], text: 'hello' },
     replace({ done: true }),
   );
 
-  t.deepEqual(updated, { done: true });
+  t.deepEqual(state, { done: true });
 });
 
 test('can update a single value in an array', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     [10, 9, 8, 7],
     setIn(1, 999),
   );
 
-  t.deepEqual(updated, [10, 999, 8, 7]);
+  t.deepEqual(state, [10, 999, 8, 7]);
 });
 
 test('can select deeply and update an item', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     { a: { b: { c: true } } },
     select('a.b.c', replace(false)),
   );
 
-  t.deepEqual(updated, { a: { b: { c: false } } });
+  t.deepEqual(state, { a: { b: { c: false } } });
 });
 
 test('can replace with callback', (t) => {
-  const updated = deepImmutable(
+  const state = compose(
     [10, 9, 8, 7],
     setIn(1, replace((old) => old + 1)),
   );
 
-  t.deepEqual(updated, [10, 10, 8, 7]);
+  t.deepEqual(state, [10, 10, 8, 7]);
 });
 
 test('can compose multiple deep updates together', (t) => {
-  const state = deepImmutable({
+  const state = compose({
     a: {
       b: true,
       c: [1, 2, 3],
@@ -94,7 +97,7 @@ test('can compose multiple deep updates together', (t) => {
 });
 
 test('can perform multiple updates on a single point', (t) => {
-  const state = deepImmutable({ value: 1 }, select('value', collect([
+  const state = compose({ value: 1 }, select('value', collect([
     replace(v => v * 5),
     replace(v => v - 1),
     replace(v => v * v),
@@ -104,7 +107,37 @@ test('can perform multiple updates on a single point', (t) => {
 });
 
 test('can concat arrays', (t) => {
-  const state = deepImmutable([1, 2, 3], concat([4, 5, 6]));
+  const state = compose([1, 2, 3], concat([4, 5, 6]));
+
+  t.deepEqual(state, [1, 2, 3, 4, 5, 6]);
+});
+
+test('can splice into an array to add elements', (t) => {
+  const state = compose([1, 2, 5, 6], splice(2, 0, [3, 4]));
+
+  t.deepEqual(state, [1, 2, 3, 4, 5, 6]);
+});
+
+test('can splice to delete items in an array', (t) => {
+  const state = compose([1, 2, 999, 1000, 3, 4], splice(2, 2));
+
+  t.deepEqual(state, [1, 2, 3, 4]);
+});
+
+test('can operate on a range of an array', (t) => {
+  const state = compose([1, 2, 3, 4, 5, 6], range(1, 3, map((value) => value * 2)));
+
+  t.deepEqual(state, [1, 4, 6, 8, 5, 6]);
+});
+
+test('can use range to delete items in an array', (t) => {
+  const state = compose([1, 2, 99, 100, 3, 4], range(2, 2, replace([])));
+
+  t.deepEqual(state, [1, 2, 3, 4]);
+});
+
+test('can use range to add items', (t) => {
+  const state = compose([1, 2, 5, 6], range(2, 0, replace([3, 4])));
 
   t.deepEqual(state, [1, 2, 3, 4, 5, 6]);
 });
