07e0e514a33108d4444e44349f584c6d15fa7a04
Alex Barry <alex.barry@gmail.com>
2021-04-06 16:16:03 -0400

    Remove splice, use range instead

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-export const compose = (state, immutableAction) => typeof immutableAction === 'function'
+export const composable = (state, immutableAction) => typeof immutableAction === 'function'
   ? immutableAction(state)
   : immutableAction;
 
 export const merge = (immutableAction) => (state) => ({
   ...state,
-  ...compose(state, immutableAction),
+  ...composable(state, immutableAction),
 });
 
 export const concat = (immutableAction) => (state) => state
-  .concat(compose(state, immutableAction));
+  .concat(composable(state, immutableAction));
 
 export const setIn = (key, immutableAction) => (state) => {
   const value = Array.isArray(state)
     ? [...state]
     : { ...state };
 
-  value[key] = compose(value[key], immutableAction);
+  value[key] = composable(value[key], immutableAction);
 
   return value;
 };
@@ -26,12 +26,12 @@ export const replace = (object) => (previous) => typeof object === 'function'
 
 export const selectArray = (path, immutableAction) => (state) => {
   if (path.length === 0) {
-    return compose(state, immutableAction);
+    return composable(state, immutableAction);
   }
 
   const key = path[0];
 
-  return compose(state, setIn(
+  return composable(state, setIn(
     key,
     selectArray(path.slice(1), immutableAction)
   ));
@@ -44,23 +44,23 @@ export const select = (path, immutableAction) => selectArray(
 
 export const selectAll = (pathsWithActions) => (state) => {
   return Object.keys(pathsWithActions).reduce((memo, path) => {
-    return compose(memo, select(path, pathsWithActions[path]));
+    return composable(memo, select(path, pathsWithActions[path]));
   }, state);
 };
 
 export const collect = (immutableActions) => (state) => {
   return immutableActions.reduce(
-    (memo, action) => compose(memo, action),
+    (memo, action) => composable(memo, action),
     state,
   );
 };
 
 export const map = (fn) => replace(
-  array => array.map((value, index) => compose(value, fn(value, index)))
+  array => array.map((value, index) => composable(value, fn(value, index)))
 );
 
 export const range = (start, length, immutableAction) => state => (
   state.slice(0, start)
-    .concat(compose(state.slice(start, start + length), immutableAction))
+    .concat(composable(state.slice(start, start + length), immutableAction))
     .concat(state.slice(start + length))
 );
